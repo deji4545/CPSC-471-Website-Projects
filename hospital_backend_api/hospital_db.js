536052231445e3a098e4c52dd8fa71ac466340e6
kravex911 <43700159+kravex911@users.noreply.github.com>
2021-12-16 14:26:23 -0700

    changed localhost

--- hospital_backend_api/hospital_db.js
+++ hospital_backend_api/hospital_db.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     database: 'healthcare_database',
     user: 'root',
     password: 'password',
