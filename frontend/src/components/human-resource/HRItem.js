aaa23bbc05e89b46df60baaa73690d4d8a4b71ad
kravex911 <43700159+kravex911@users.noreply.github.com>
2021-12-16 23:05:13 -0700

    hr

--- frontend/src/components/human-resource/HRItem.js
+++ frontend/src/components/human-resource/HRItem.js
@@ -1,32 +1,27 @@
 import React from 'react'
 import Button from '../Button'
 import {Link } from 'react-router-dom'
-const HRItem = () => {
-    const id = 1245
+const HRItem = ({employee, deleteEmployee}) => {
+    const id = employee.id_no
     return (
         <div className="item" style={{ fontSize: "12.5px" }}>
             <table >
                 <tbody>
                     <tr>
-                        <td><b>ID:</b><br /> Data </td>
-                        <td><b>SIN:</b><br />Data</td>
-                        <td><b>Name:</b><br />Data</td>
-                        <td><b>Gender:</b><br />Data</td>
+                        <td><b>ID:</b><br /> {employee.id_no} </td>
+                        <td><b>SIN:</b><br />{employee.sin}</td>
+                        <td><b>Name:</b><br />{employee.fname +" "+employee.m_initial+" "+employee.lname}</td>
+                        <td><b>Gender:</b><br />{employee.gender}</td>
                     </tr>
                     <tr>
-                        <td><b>DOB:</b><br />s</td>
-                        <td style={{ columnSpan: "1" }}><b>Telephone #:</b><br />Data</td>
-                        <td colSpan="2" ><b>Address:</b><br /> Data</td>
-                    </tr>
-                    <tr>
-                        <td><b>Position:</b><br /> Data</td>
-                        <td><b>Start date:</b><br /> Data</td>
-                        <td><b>Salary:</b><br /> Data</td>
-                        <td><b>Benefits:</b><br /> Data</td>
+                        <td><b>DOB:</b><br />{employee.dob.substring(0,10)}</td>      
+                        <td colSpan="1" ><b>Address:</b><br /> {employee.address}</td>
+                        <td><b>Position:</b><br /> {employee.job}</td>
+                        <td><b>Salary:</b><br /> {employee.salary}</td>           
                     </tr>
                     <tr>
                         <td><Link to={{pathname:`/edit/hr/${id}`}}><Button text="Edit information" /></Link></td>
-                        <td><Button text="Delete" /></td>
+                        <td><Button text="Delete" onClick={ ()=>deleteEmployee(id) }/></td>
                         <td><Link to={{pathname:`/employee/${id}`}}><Button text="View details" /></Link></td>
                     </tr>
                 </tbody>
