4645186e850036a4ccfbf6449ceff2ebc09da373
deji4545 <oshoad3000@gmail.com>
2021-12-16 17:08:43 -0700

    Update hospital.js

--- hospital_backend_api/hospital_connect.js
+++ hospital_backend_api/hospital_connect.js
@@ -126,10 +126,15 @@ app.post('/api/HR/staff/dependents', (req, res) => {
 
 //Retrieve Patient Information For reception 
 app.get('/api/reception/patient', (req, res) => {
-    let sql = "SELECT * FROM PATIENT";
+    let sql = "SELECT P.healthcard_no, P.fname, P.m_initial, P.lname, P.gender, P.dob, P.address,\
+    P.phone_number, P.insurance_provider, ward_name, E.fname as 'E.fname',\
+    E.lname as 'E.lname', E.m_initial as 'E.m_initial', E.lname as 'E.lname', E.relationship as 'E.relationship',\
+    E.phone_number as 'E.phone_number'\
+    FROM PATIENT as P, MEDICAL_WARD as M, EMERGENCY_CONTACT as E\
+    WHERE P.ward_no = M.ward_no and P.healthcard_no = E.healthcard_no";
+
     toConnect(sql, res);
 });
-
 
 // Retrieve Emergency Contact for a specific patient
 app.get('/api/reception/patient/emergecy_contact/:id', (req, res) => {
